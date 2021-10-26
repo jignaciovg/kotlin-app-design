7f0c00210901d49000b715fe5d811a9a8670bc1d
jignaciovg <ignaciovaqdevelop@hotmail.com>
2021-10-26 16:21:12 -0500

    app con diseño

---
+++ app/src/main/java/com/vaqueiro/app_constraint_design/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vaqueiro.app_constraint_design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
